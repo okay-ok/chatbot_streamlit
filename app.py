147d935044ce19472ba8d079d0ef9c763635a1aa
Hiren <115985560+okay-ok@users.noreply.github.com>
2024-03-01 23:49:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.title("ChatGPT-like clone")
 client = openai.OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
+    st.session_state["openai_model"] = "gpt-3.5"
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
