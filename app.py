162af4aa216c2b6940fa6b8cf04b5e08a2b0c049
Hiren <115985560+okay-ok@users.noreply.github.com>
2024-03-01 23:44:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 st.title("ChatGPT-like clone")
 
-client = openai.OpenAI(api_key="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")
+client = openai.OpenAI(api_key=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)
 
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
